6797c738360ca8846999e215831ec2533bded104
Bohdan <bohdansharipov@gmail.com>
2018-03-22 18:39:56 -0400

    Initial application generated by JHipster-4.14.1

---
+++ src/main/java/com/bohdan/app/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.bohdan.app.service.dto;
---
+++ src/main/java/com/bohdan/app/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package com.bohdan.app.service.mapper;
